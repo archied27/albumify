fb452a0624d5f70d49c93baffb597c7474f79556
archied27 <archiedolman@gmail.com>
2026-03-19 13:52:35 +0000

    storing album info in db added

--- src/backend/app/db/db_init.py
+++ src/backend/app/db/db_init.py
@@ -1,2 +1,80 @@
 import sqlite3
 
+"""
+Initilaise the database to store album information from APIs
+"""
+
+def createAlbumDb(cur):
+    # creates album table
+    # stores spotify api details
+    cur.execute("CREATE TABLE IF NOT EXISTS albums(\
+        id TEXT PRIMARY KEY,\
+        url TEXT,\
+        album_popularity INTEGER,\
+        album_name TEXT NOT NULL,\
+        release_date DATETIME,\
+        cover_path TEXT,\
+        avg_track_duration REAL)")
+
+def createArtistDb(cur):
+    # creates artist table
+    # stores spotify api artist details
+    cur.execute("CREATE TABLE IF NOT EXISTS artists(\
+        id TEXT PRIMARY KEY,\
+        url TEXT,\
+        artist_popularity INTEGER,\
+        artist_name TEXT NOT NULL)")
+
+def createGenreDb(cur):
+    # creates genres table
+    # stores genre names used for many to many relation with genres and albums
+    cur.execute("CREATE TABLE IF NOT EXISTS genres(\
+        id INTEGER PRIMARY KEY AUTOINCREMENT,\
+        genre_name TEXT UNIQUE NOT NULL)")
+
+def createArtistGenreDb(cur):
+    # creates artist_genre table
+    # used to link multiple genres to one artists
+    cur.execute("CREATE TABLE IF NOT EXISTS artist_genres(\
+        artist_id TEXT NOT NULL,\
+        genre_id INTEGER NOT NULL,\
+        PRIMARY KEY (artist_id, genre_id),\
+        FOREIGN KEY(artist_id) REFERENCES artist(id) ON DELETE CASCADE,\
+        FOREIGN KEY(genre_id) REFERENCES genres(id) ON DELETE CASCADE)")
+
+def createAlbumArtistsDb(cur):
+    # creates album_artists table
+    # used to link albums with artists (many to many)
+    cur.execute("CREATE TABLE IF NOT EXISTS album_artists(\
+        album_id TEXT NOT NULL,\
+        artist_id TEXT NOT NULL,\
+        PRIMARY KEY (album_id, artist_id),\
+        FOREIGN KEY(album_id) REFERENCES album(id) ON DELETE CASCADE,\
+        FOREIGN KEY(artist_id) REFERENCES artist(id) ON DELETE CASCADE)")
+
+def deleteAllTables(cur):
+    cur.execute("DROP TABLE IF EXISTS artist_genres")
+    cur.execute("DROP TABLE IF EXISTS album_artists")
+    cur.execute("DROP TABLE IF EXISTS artists")
+    cur.execute("DROP TABLE IF EXISTS genres")
+    cur.execute("DROP TABLE IF EXISTS albums")
+
+def createAllTables(cur):
+    createAlbumDb(cur)
+    createGenreDb(cur)
+    createArtistDb(cur)
+    createAlbumArtistsDb(cur)
+    createArtistGenreDb(cur)
+
+if __name__ == "__main__":
+    con = sqlite3.connect('src/backend/app/db/albumify.db')
+    con.execute("PRAGMA foreign_keys = ON")
+    cur = con.cursor()
+
+    # delete and create all databases for a full reset
+    deleteAllTables(cur)
+
+    createAllTables(cur)
+
+    con.commit()
+    con.close()
--- src/backend/app/services/spotify/spotify_service.py
+++ src/backend/app/services/spotify/spotify_service.py
@@ -1,28 +1,89 @@
 from app.services.spotify.auth import login, getToken
 import requests
+import sqlite3
 
 BASE_URL = "https://api.spotify.com/v1"
 
+def updateAlbumsDb(album):
+    print(f"Adding {album['name']} by {album['artists'][0]['name']}")
+    
+    # updates the album table in database with albums info
+    with sqlite3.connect("app/db/albumify.db") as conn:
+        cur = conn.cursor()
+
+        # artist = artistsGenreAndPop(album['artists'])
+
+        # update albums table 
+        cur.execute("INSERT INTO albums \
+            (id, url, album_popularity, album_name, release_date, cover_path, avg_track_duration)\
+            VALUES(?, ?, ?, ?, ?, ?, ?);",
+            (album['id'], album['external_urls']['spotify'], album['popularity'], album['name'], 
+            album['release_date'], album['images'][0]['url'], avgTrackDuration(album['tracks']['items'])) )
+
+        #conn.commit()
+        #conn.close()
+
+def artistsGenreAndPop(artists):
+    # takes an array of artists and returns their genres and avg popularity
+    genres = []
+    totalPop = 0
+
+    for artist in artists:
+        info = getArtistInfo(artist['id'])
+        totalPop += info['popularity']
+        genres = genres + info['genres'] # add all genres from all artists
+
+    return {"genres": genres, "popularity":totalPop/len(artists)}
+
+def avgTrackDuration(tracks):
+    # takes an array of tracks and returns their average duration
+    # in seconds
+    totalSeconds = 0
+    trackNo = 0
+    for track in tracks:
+        totalSeconds += track['duration_ms']
+        trackNo += 1
+
+    return totalSeconds/trackNo
+
 def getSavedAlbums():
+    # calls updateAlbumsDb with all albums in users library
     token = getToken()
     if token is not None:
-        # get users albums
+        # set up requests url, headers and params
         url = BASE_URL + "/me/albums"
         headers = {"Authorization": f"Bearer {token}"}
         params = {"limit": 50, "offset": 0}
 
-        savedAlbums = []
-
+        # loop until all users albums fetched
         while url:
             resp = requests.get(url, headers=headers, params=params)
             data = resp.json()
-            url = data["next"]
+            url = data["next"] # set up next url to fetch (has params already set)
+
             for i in data["items"]:
-                savedAlbums.append(i['album']['name'])
+                # for every album
+                album = i['album']
+                updateAlbumsDb(album)
             
             params = {}
 
-        return savedAlbums
+        return {"message": "db updated"}
 
     else:
-        return {"error": "authentication failed"}
+        return {"message": "not authenticated"}
+
+def getArtistInfo(id):
+    # returns spotify api artist info
+    token = getToken()
+    if token is not None:
+        url = BASE_URL + f"/artists/{id}"
+        headers = {"Authorization": f"Bearer {token}"}
+
+        resp = requests.get(url, headers=headers)
+        data = resp.json()
+
+        return {"message": "success", "genres": data['genres'], "popularity": data['popularity']}
+        
+    else:
+        return {"message": "not authenticated"}
